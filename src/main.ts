c95a075689791b281689c40ac539cb3ebc67d82c
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-10-13 09:53:54 +0530

    Summarize Installation versions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import { Platform, Properties } from './types'
 import install_toolchain from './toolchain'
+import { exec } from '@actions/exec'
 
 /**
  * The main function for the action.
@@ -23,6 +24,38 @@ export async function run(): Promise<void> {
 
     // install rust toolchain
     await install_toolchain(properties, platform)
+
+    // Sumarize installation with tool versions
+    let rustc_version = ''
+    let rustup_version = ''
+
+    await exec('rustc', ['--version'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          rustc_version += data.toString()
+        }
+      }
+    })
+
+    await exec('rustup', ['--version'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          rustup_version += data.toString()
+        }
+      }
+    })
+
+    // Create a Table Summary
+    const version_info = [
+      [
+        { data: 'Tool', header: true },
+        { data: 'Version', header: true }
+      ],
+      ['rustc', rustc_version],
+      ['rustup', rustup_version]
+    ]
+
+    core.summary.addTable(version_info).write()
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
